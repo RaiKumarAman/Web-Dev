e73ba53291ad7112e26fa50971a8461c54853bc6
Aman Kumar Rai <123739038+RaiKumarAman@users.noreply.github.com>
2026-05-05 11:21:02 +0000

    priitve and non primitive

---
+++ Javascript/non-primitve.js
@@ -0,0 +1,23 @@
+// Object
+
+let user={
+    firstname:"Thor",
+    lastname:"Odinson",
+    isworthy:true
+};
+
+console.log(user.firstname);
+
+user.isworthy=false;
+console.log(user.isworthy);
+
+console.log(user);
+
+// Array
+let hero=["Ironman", "Thor", "Captain America"]
+console.log(hero[1]);
+
+let other=["steve", false, 70];
+console.log(other[0]);
+console.log(other[1]);
+console.log(other[2]);
---
+++ Javascript/primitive.js
@@ -0,0 +1,28 @@
+let balance = 120;
+let anotherBalance= new Number(120);
+console.log(anotherBalance);
+console.log(typeof balance);
+console.log(typeof anotherBalance);
+
+// Strings
+
+let myString="hola";
+let myString2=new String("Hitesh");
+
+console.log(myString);
+console.log(myString2);
+console.log(`Hello ${myString}`);
+
+// Undefined
+let firstname;
+console.log(firstname);
+
+// Symbol
+let sm1=Symbol();
+let sm2=Symbol();
+
+console.log(sm1);
+console.log(sm2);
+console.log(sm2==sm1);
+
+
